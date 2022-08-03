749a2b50ec73593847eee04a1a7181f18176d524
mukhadm <svcredd@yahoo.com>
2022-08-03 19:27:09 +0300

    03082022

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const inputButton = document.querySelector('.create__note'),
       modal = document.querySelector('.modal'),
       modalClose = document.querySelector('.modal__close'),
       sendButton = document.querySelector('.btn__send'),
-      input = document.querySelector('.note');
+      input = document.querySelector('.note'),
+      hours = document.querySelector('.hours'),
+      minutes = document.querySelector('.minutes'),
+      seconds = document.querySelector('.seconds');
 
 inputButton.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -23,26 +26,25 @@ const notesDB = {
     input.value = '';
     event.preventDefault();
 });
+console.log(notesDB);
 
-console.log(notesDB)
 
+let localeHours = new Date().toLocaleTimeString().slice(0, -6),
+      localeMinutes = new Date().toLocaleTimeString().slice(3, -3),
+      localeSeconds = new Date().toLocaleTimeString().slice(6),
+      timeInterval = setInterval(clockUpdates, 1000);
 
-function createCounter(){
-    let counter = 0;
-    const myFunction = function(){
-        counter = counter +1;
-        return counter;
-    }
-    return myFunction;
-};
+      clockUpdates();
 
-const increment = createCounter();
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
-
-console.log(c1, c2, c3);
-
+      function clockUpdates(){
+  let localeHours = new Date().toLocaleTimeString().slice(0, -6),
+      localeMinutes = new Date().toLocaleTimeString().slice(3, -3),
+      localeSeconds = new Date().toLocaleTimeString().slice(6);
+      hours.innerHTML = localeHours;
+      minutes.innerHTML = localeMinutes;
+      seconds.innerHTML = localeSeconds;
+      };
+    
 
 
 
