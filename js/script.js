8be39a00933cbcad921af8ba7f7b387cb9c73a75
mukhadm <svcredd@yahoo.com>
2022-08-11 12:10:42 +0300

    server

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+
 const inputButton = document.querySelector('.create__note'),
       modal = document.querySelector('.modal'),
       modalClose = document.querySelector('.modal__close'),
+      form = document.querySelector('form'),
+      message = {loading: 'loading', success: 'well done', failure: 'error'},
       sendButton = document.querySelector('.btn__send'),
       input = document.querySelector('.note'),
       hours = document.querySelector('.hours'),
@@ -21,12 +25,43 @@ const notesDB = {
     note: {}
     };
 
-    sendButton.addEventListener('click', (event) => {
-    notesDB.note = document.querySelector('.note').value;
-    input.value = '';
-    event.preventDefault();
-});
-console.log(notesDB);
+//     sendButton.addEventListener('click', (event) => {
+//     notesDB.note = noteValue;
+//     input.value = '';
+//     event.preventDefault();
+// });
+
+postData(form);
+
+function postData(form) {
+    form.addEventListener('submit', (e) => {
+        const noteValue = document.querySelector('.note').value;
+        e.preventDefault();
+        // input.value = '';
+        notesDB.note = noteValue;
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        // request.setRequestHeader('Content-type', 'multipart/form-data');
+        const formData = new FormData(form);
+        request.send(formData);
+
+        const statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);
+
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+            } else {
+                statusMessage.textContent = message.failure;
+            }
+        });
+    });
+};
+
 
 
 let localeHours = new Date().toLocaleTimeString().slice(0, -6),
@@ -34,17 +69,18 @@ let localeHours = new Date().toLocaleTimeString().slice(0, -6),
       localeSeconds = new Date().toLocaleTimeString().slice(6),
       timeInterval = setInterval(clockUpdates, 1000);
 
-      clockUpdates();
-
       function clockUpdates(){
-  let localeHours = new Date().toLocaleTimeString().slice(0, -6),
+      localeHours = new Date().toLocaleTimeString().slice(0, -6),
       localeMinutes = new Date().toLocaleTimeString().slice(3, -3),
       localeSeconds = new Date().toLocaleTimeString().slice(6);
       hours.innerHTML = localeHours;
       minutes.innerHTML = localeMinutes;
       seconds.innerHTML = localeSeconds;
       };
-    
 
+      clockUpdates();
+
+
+});
 
 
