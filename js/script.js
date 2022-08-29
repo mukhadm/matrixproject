289ce618d9516eee42b48ee49d1fd54d19d57f36
mukhadm <svcredd@yahoo.com>
2022-08-29 11:34:12 +0300

    v0,1

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ const inputButton = document.querySelector('.create__note'),
       modalClose = document.querySelector('.modal__close'),
       form = document.querySelector('form'),
       message = {loading: 'loading', success: 'well done', failure: 'error'},
-      sendButton = document.querySelector('.btn__send'),
-      input = document.querySelector('.note'),
+    //   sendButton = document.querySelector('.btn__send'),
+    //   input = document.querySelector('.note'),
       hours = document.querySelector('.hours'),
       minutes = document.querySelector('.minutes'),
-      seconds = document.querySelector('.seconds');
-
+      seconds = document.querySelector('.seconds'),
+      notesDB = {
+        note: {}
+        };
+      
 inputButton.addEventListener('click', () => {
     modal.style.display = 'block';
 });
@@ -21,48 +24,49 @@ modalClose.addEventListener('click', () => {
     modal.style.display = 'none';
 }); 
 
-const notesDB = {
-    note: {}
-    };
-
-//     sendButton.addEventListener('click', (event) => {
-//     notesDB.note = noteValue;
-//     input.value = '';
-//     event.preventDefault();
-// });
-
+const jsonData = async (url, data) => {
+  const res = await fetch(url, {
+     method: "POST",
+     headers: {
+             'Content-type': 'application/json'
+         },
+        body: data
+    });
+    return await res.json();
+}
+    
 postData(form);
 
 function postData(form) {
     form.addEventListener('submit', (e) => {
         const noteValue = document.querySelector('.note').value;
         e.preventDefault();
-        // input.value = '';
         notesDB.note = noteValue;
-
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        // request.setRequestHeader('Content-type', 'multipart/form-data');
+        
         const formData = new FormData(form);
-        request.send(formData);
 
         const statusMessage = document.createElement('div');
         statusMessage.classList.add('status');
         statusMessage.textContent = message.loading;
         form.append(statusMessage);
 
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                statusMessage.textContent = message.success;
-            } else {
-                statusMessage.textContent = message.failure;
-            }
+        const object = {};
+        formData.forEach(function(value, key){
+            object[key] = value;
+        });
+
+        jsonData('http://localhost:3000/requests', JSON.stringify(object))
+        .then(data => {
+            console.log(data);
+            statusMessage.textContent = message.success;
+        }).catch(() => {
+            statusMessage.textContent = message.failure;
+        })
+        .finally(() => {
+            form.reset();
         });
     });
 };
-
-
 
 let localeHours = new Date().toLocaleTimeString().slice(0, -6),
       localeMinutes = new Date().toLocaleTimeString().slice(3, -3),
@@ -76,11 +80,56 @@ let localeHours = new Date().toLocaleTimeString().slice(0, -6),
       hours.innerHTML = localeHours;
       minutes.innerHTML = localeMinutes;
       seconds.innerHTML = localeSeconds;
+      timeInterval;
       };
 
       clockUpdates();
 
+      fetch('db.json')
+      .then(data => data.json())
+      .then(res => console.log(res));
 
+
+//dateCalculator
+
+const calcButton = document.querySelector('.calc__button');
+const calcInput = document.querySelector('.calculator');
+
+
+calcButton.addEventListener('click', () => {
+    calculateDate();
+    setInterval(timeRemaining, 1000);
+});
+
+function calculateDate(){
+    let result = timeRemaining();
+    let div = document.createElement('div');
+    div.className = "time__Result";
+    div.innerHTML = `${result}`;
+    calcInput.insertAdjacentElement('afterend', div);
+};
+
+function timeRemaining(){
+   const inputDate = document.querySelector('.calc__input').value; //2023-11-17T19:57
+   const nowDate = new Date(); //Fri Aug 19 2022 16:20:56 GMT+0300 (за східноєвропейським літнім часом)
+   const futureDate = new Date(inputDate);
+   const dif = Math.abs(new Date().getTime() - new Date(inputDate).getTime());
+   const diff = new Date(dif);
+   
+   const days = diff.getDate() - 1;
+   const years = diff.getFullYear() - 1970;
+   const months = diff.getMonth();
+   const hours = diff.getHours() - 3;
+   const seconds = diff.getSeconds();
+   const result = (`${years} years, ${months} months, ${days} days, ${hours} hours, ${seconds} seconds`);
+   return result
+};
+
+
+// const seconds = parseInt((duration/1000)%60);
+// const minutes = parseInt((duration/(1000*60))%60);
+// const hours = parseInt((duration/(1000*60*60))%24);
+// const days = parseInt(duration/(1000*60*60*24));
 });
 
 
